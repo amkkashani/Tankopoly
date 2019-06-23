5e2f209addb9fd665e06a2a1da037b10bbfeaf67
kash <amkkashani@gmail.com>
2019-06-23 18:02:10 +0430

    add first scripts

---
+++ Scripts/destroyable/castle_code.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class castle_code : destroiable
+{
+    public Mesh salem;
+    public Mesh halfDestried;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<MeshFilter>().mesh = salem;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Healthset(float amount)
+    {
+        base.Healthset(amount);
+        if (health <= 50)
+        {
+            this.GetComponent<MeshFilter>().mesh = halfDestried;
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "projectile")
+        {
+            // health -= collision.gameObject.GetComponent<ShellExplosion>().m_MaxDamage/3;
+            Healthset(other.GetComponent<ShellExplosion>().m_MaxDamage / 5);
+        }
+    }
+
+}
---
+++ Scripts/obstacle/pitch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pitch : MonoBehaviour
+{
+    public int amoutReduce = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<TankMovement>().m_Speed *= amoutReduce;
+            other.gameObject.GetComponent<TankMovement>().m_TurnSpeed *= amoutReduce;
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<TankMovement>().m_Speed /= amoutReduce;
+            other.gameObject.GetComponent<TankMovement>().m_TurnSpeed /= amoutReduce;
+
+
+        }
+    }
+
+}
